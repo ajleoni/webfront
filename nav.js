dc872ff2f6d8cb3934f286d1106be4fac53ba2fe
Alessandro J. Leoni <1433131+ajleoni@users.noreply.github.com>
2021-12-22 22:12:41 +0100

    added Mobile Navigation

---
+++ nav.js
@@ -0,0 +1,14 @@
+const topNav = document.querySelector(".top-navigation");
+const navToggle = document.querySelector(".nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = topNav.getAttribute("data-visible");
+    console.log(topNav);
+    console.log(visibility);
+    if (visibility === "false") {
+        topNav.setAttribute("data-visible", true);
+    } else if (visibility === "true") {
+        topNav.setAttribute("data-visible", false);
+    }
+}
+);
